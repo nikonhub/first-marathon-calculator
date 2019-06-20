6a4f7d2cdb91e9d4f5c0ee18f5cc894eae60beab
Nik <nikosmonaut@protonmail.ch>
2019-06-21 01:36:39 +0200

    Save data to localStorage

--- src/views/MarathonPlan/index.js
+++ src/views/MarathonPlan/index.js
@@ -3,16 +3,19 @@ import TimeForm from './TimeForm';
 import PlanCalendar from './PlanCalendar';
 
 const MarathonPlan = () => {
-    const [bestTenKTime, setBestTenKTime] = useState(null);
-    const [unit, setUnit] = useState('km');
-    const [raceDate, setRaceDate] = useState(null);
+    const [bestTenKTime, setBestTenKTime] = useState(localStorage.getItem('bestTenKTime'));
+    const [unit, setUnit] = useState(localStorage.getItem('unit'));
+    const [raceDate, setRaceDate] = useState(localStorage.getItem('raceDate'));
 
     return (
         <React.Fragment>
             <TimeForm onSubmit={({ bestTenKTime, unit, raceDate }) => {
                 setBestTenKTime(bestTenKTime);
+                localStorage.setItem('bestTenKTime', bestTenKTime);
                 setUnit(unit);
+                localStorage.setItem('unit', unit);
                 setRaceDate(raceDate);
+                localStorage.setItem('raceDate', raceDate);
             }} />
             <PlanCalendar unit={unit} bestTenKTime={bestTenKTime} raceDate={raceDate} />
         </React.Fragment>
